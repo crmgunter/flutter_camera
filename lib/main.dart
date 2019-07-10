77b2068fe207d03ca487e2fcaa349603a06d6a29
crmgunter <crmgunter@gmail.com>
2019-07-09 20:07:53 -0400

    playing with icons and colors

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,21 @@ import 'package:flutter/material.dart';
 
 void main() {
 
-  var app = MaterialApp(home: Text('Hey whaddup'));
+  final barColor = const Color(0xFFD63031);
+  final bgColor = const Color(0xFF8395A7);
+
+  var app = MaterialApp(home: Scaffold(
+    floatingActionButton: FloatingActionButton(
+      child: Icon(Icons.build, color: bgColor),
+      backgroundColor: barColor,
+      onPressed: () {},
+    ),
+    backgroundColor: bgColor,
+    appBar: AppBar(
+      title: Text('Instagram'),
+      backgroundColor: barColor,
+    ),
+  ));
 
   runApp(app);
 }
